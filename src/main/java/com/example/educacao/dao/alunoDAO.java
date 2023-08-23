c2afd4432dc45f60edf0f85d03319673db963399
645151 mn <marciohiromi@hotmail.com>
2023-08-23 00:58:22 +0000

    exerc1

---
+++ src/main/java/com/example/educacao/dao/alunoDAO.java
@@ -0,0 +1,11 @@
+package com.example.educacao.dao;
+
+import com.example.educacao.model.Aluno;
+
+public class alunoDAO extends {
+    public void insert (Aluno Aluno){
+        var sql = "insert into aluno (nome) values (?)";
+        var statement
+    
+}
+}
