46c6f5eec72351cdfab1e45c1a6b806c5064e7e7
645151 mn <marciohiromi@hotmail.com>
2023-08-26 02:28:04 +0000

    25AGOSTONOVO

--- src/main/java/com/example/AppArray2.java
+++ src/main/java/com/example/AppArray2.java
@@ -1,13 +1,21 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class AppArray2 {
     public static void main(String[] args) {
-        int [] numeros = {1, 2, 3, 4, 5}; 
+        System.out.println();
+        int [] numeros = {10, 20, 30, 40, 50}; 
 
             for (int i = 0; i < 5; i++){
-            System.out.printf("%d - " , numeros[i]);
+
+                
+            System.out.printf("%d " , numeros[i]);
+            //System.out.println(Arrays.toString(numeros));
             }
         
     
 }
 }
+       
+
---
+++ src/main/java/com/example/AppArrays3.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class AppArrays3 {
+    public static void main(String[] args) {
+        String [] car = {"HONDA", "TOYOTA", "FORD", "VOLKS"};
+        for (int i = 0; i < car.length; i++) {
+
+        System.out.printf("%s ", car [i]);
+        
+        //System.out.println(car[i]);
+
+    }
+    
+}
+}
