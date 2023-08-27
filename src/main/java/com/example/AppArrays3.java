facd490c792a03083265965b2c2eca6c538bedb6
645151 mn <marciohiromi@hotmail.com>
2023-08-27 01:29:28 +0000

    26AGOSTO*

--- src/main/java/com/example/AppArrays3.java
+++ src/main/java/com/example/AppArrays3.java
@@ -2,14 +2,18 @@ package com.example;
 
 public class AppArrays3 {
     public static void main(String[] args) {
-        String [] car = {"HONDA", "TOYOTA", "FORD", "VOLKS"};
-        for (int i = 0; i < car.length; i++) {
+       // String [] car = {"HONDA", "TOYOTA", "FORD", "VOLKS"};
+       // for (int i = 0; i < car.length; i++) {
 
-        System.out.printf("%s ", car [i]);
+      //  System.out.printf("%s ", car [i]);
         
         //System.out.println(car[i]);
 
+        String [] car = {"HONDA", "TOYOTA", "YAMAHA", "SUZUKI"};
+        for (int i = 0; i < car.length; i++) {
+        System.out.printf("%s ", car [i]);
+        }
     }
     
 }
-}
+
